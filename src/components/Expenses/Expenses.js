82030f1a2479d381a5d7e3a5ada5e0f6e60581e7
ArizonaPeachTea <zuxingp888@gmail.com>
2021-04-27 22:14:45 -0500

    changing component declaration to arrow function syntax

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -2,7 +2,7 @@ import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
 import './Expenses.css';
 
-function Expenses(props) {
+const Expenses = (props) => {
     const expenseItems = props.expenses.map(expense => {
         return (
             <ExpenseItem id={expense.id} title={expense.title} amount={expense.amount} date={expense.date} />
