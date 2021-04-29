8d077614d7921aec8c506cfc7abb6e9ca66c136b
ArizonaPeachTea <zuxingp888@gmail.com>
2021-04-29 13:15:22 -0500

    'add new expense' and 'cancel' logic

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,14 +1,28 @@
+import { useState } from 'react';
+
 import ExpenseForm from './ExpenseForm';
 import './NewExpense.css';
 
 const NewExpense = (props) => {
+    const [isEditing, setIsEditing] = useState(false);
+
     const onSaveExpenseFormHandler = (savedExpenseData) => {
         props.onNewExpense(savedExpenseData);
+        setIsEditing(false);
+    };
+
+    const startEditingHandler = () => {
+        setIsEditing(true);
+    };
+
+    const stopEditingHandler = () => {
+        setIsEditing(false);
     };
 
     return (
         <div className="new-expense">
-            <ExpenseForm onSavedData={onSaveExpenseFormHandler} />
+            {!isEditing && <button onClick={startEditingHandler}>Add New Expenses</button>}
+            {isEditing && <ExpenseForm onSavedData={onSaveExpenseFormHandler} onCancel={stopEditingHandler} />}
         </div>
     );
 };
