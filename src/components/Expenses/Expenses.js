ed3e11f7bfad73e5ba3764f1c2a3e0303ee0812a
ArizonaPeachTea <zuxingp888@gmail.com>
2021-04-29 10:33:07 -0500

    lifting up the states

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,18 +1,30 @@
+import { useState } from 'react';
+
 import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
+import ExpensesFilter from './ExpensesFilter';
 import './Expenses.css';
 
 const Expenses = (props) => {
+    const [filteredYear, setFilteredYear] = useState('2020');
+
     const expenseItems = props.expenses.map(expense => {
         return (
             <ExpenseItem key={expense.id} title={expense.title} amount={expense.amount} date={expense.date} />
         );
     });
 
+    const filterChangeHandler = selectedYear => {
+        setFilteredYear(selectedYear);
+    };
+
     return (
-        <Card className="expenses">
-            {expenseItems}
-        </Card>
+        <div>
+            <Card className="expenses">
+                <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
+                {expenseItems}
+            </Card>
+        </div>
     );
 }
 
