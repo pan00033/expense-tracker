043e15a8a037b3d339b7b1042a2019c478e5000e
ArizonaPeachTea <zuxingp888@gmail.com>
2021-04-29 13:03:00 -0500

    adding additional return statements

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 
-import ExpenseItem from './ExpenseItem';
+import ExpensesList from './ExpensesList';
 import Card from '../UI/Card';
 import ExpensesFilter from './ExpensesFilter';
 import './Expenses.css';
@@ -16,17 +16,11 @@ const Expenses = (props) => {
         return expense.date.getFullYear().toString() === filteredYear;
     });
 
-    const expenseItems = filteredExpenses.length === 0 ? <p>No expenses found.</p> : filteredExpenses.map(expense => {
-        return (
-            <ExpenseItem key={expense.id} title={expense.title} amount={expense.amount} date={expense.date} />
-        );
-    });
-
     return (
         <div>
             <Card className="expenses">
                 <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-                {expenseItems}
+                <ExpensesList items={filteredExpenses} />
             </Card>
         </div>
     );
